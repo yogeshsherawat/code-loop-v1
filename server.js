d49a62f79f483848edb6d2db361bbcbb564e0bd6
bob <golusherawat@gmail.com>
2021-02-10 15:41:49 +0530

    issue resolved

--- server.js
+++ server.js
@@ -101,13 +101,14 @@ app.post('/compile', async (request, response) => {
 
 })
 
-app.get('/compile/status', async (req, res) => {
-    
-})
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
 
-app.get('/output', async (req, res) => {
-    
-})
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 app.listen(process.env.PORT || 5000, () => {
     console.log("Server Started");
